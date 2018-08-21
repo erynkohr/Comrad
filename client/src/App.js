93223bcb42cd6dadaf1ee138d609250fd40e4776
Hugo Amorim <hugo@startupdesignninja.com>
2018-08-20 20:44:08 -0600

    inital setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Admin from "./pages/Admin";
+import Calendar from "./pages/Calendar";
+import Dashboard from "./pages/Dashboard";
+import Library from "./pages/Library";
+import Login from "./pages/Login";
+import Report from "./pages/Report";
+import ShowBuilder from "./pages/ShowBuilder";
+import User from "./pages/User";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Comrad
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Admin} />
+        <Route exact path="/" component={Calendar} />
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={Library} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/" component={Report} />
+        <Route exact path="/" component={ShowBuilder} />
+        <Route exact path="/" component={User} />
+      </Switch>
+    </div>
+  </Route
+);
 
 export default App;
+
